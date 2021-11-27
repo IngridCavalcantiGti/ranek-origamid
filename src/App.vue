e53ef170abc6dc0af19f44dbfe157c75e3ad14c1
Ingrid Cavalcanti <ingridgti08@hotmail.com>
2021-11-27 17:52:59 -0300

    login

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,38 @@
 <template>
   <div id="app">
-   <TheHeader />
-   <main id="main">
-    <router-view/>
-   </main>
-    <TheFooter />
+    <TheHeader/>
+    <main id="main">
+      <transition mode="out-in">
+        <router-view/>
+      </transition>
+    </main>
+    <TheFooter/>
   </div>
 </template>
 
 <script>
-import TheHeader from '@/components/TheHeader.vue'
-import TheFooter from '@/components/TheFooter.vue'
+import TheHeader from "@/components/TheHeader.vue";
+import TheFooter from "@/components/TheFooter.vue";
 
 export default {
   components: {
     TheHeader,
     TheFooter
   }
-}
+};
 </script>
 
 <style>
-body, ul, li, h1, h2, p {
+* {
+  box-sizing: border-box;
+}
+
+body,
+ul,
+li,
+h1,
+h2,
+p {
   padding: 0px;
   margin: 0px;
 }
@@ -31,9 +42,9 @@ ul {
 }
 
 body {
-  font-family: "Avenir", Arial, Helvetica, sans-serif;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   color: #345;
-  background: url('./assets/pattern.svg') repeat top;
+  background: url("./assets/pattern.svg") repeat top;
 }
 
 a {
@@ -49,16 +60,16 @@ img {
 .btn {
   display: block;
   padding: 10px 30px;
-  background: #87F;
+  background: #87f;
   border-radius: 4px;
   color: #fff;
   text-align: center;
   font-size: 1rem;
   box-shadow: 0 4px 8px rgba(30, 60, 90, 0.2);
-  border:none;
   transition: all 0.3s;
-  font-family: "Avenir", Arial, Helvetica, sans-serif;
-  cursor:pointer;
+  border: none;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  cursor: pointer;
 }
 
 .btn:hover {
@@ -68,41 +79,54 @@ img {
 
 #app {
   display: flex;
-  flex-direction: column;
   min-height: 100vh;
+  flex-direction: column;
 }
 
 #main {
   flex: 1;
 }
 
-input, textarea {
-  border:  none;
+label {
+  margin-bottom: 5px;
+}
+
+input,
+textarea {
   border-radius: 4px;
-  transition: all 0.3s;
+  border: 1px solid white;
   padding: 15px;
   box-shadow: 0 4px 8px rgba(30, 60, 90, 0.1);
+  transition: all 0.3s;
   font-size: 1rem;
-  font-family: "Avenir", Arial, Helvetica, Arial, sans-serif;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   margin-bottom: 15px;
 }
 
-input:hover, textarea:hover, input:focus, textarea:focus{
- outline: none;
- box-shadow:0 6px 12px  rgba(30, 60, 90, 0.1);
- border-color: #87F
+input:hover,
+input:focus,
+textarea:hover,
+textarea:focus {
+  outline: none;
+  box-shadow: 0 6px 12px rgba(30, 60, 90, 0.2);
+  border-color: #87f;
 }
 
-.v-enter, .v-leave-to{
+.v-enter,
+.v-leave-to {
   opacity: 0;
 }
-.v-enter-active, .v-leave-active{
-  transition: all 0.3s;
-}
+
 .v-enter {
   transform: translate3d(0, -20px, 0);
 }
+
 .v-leave-to {
   transform: translate3d(0, 20px, 0);
+}
+
+.v-enter-active,
+.v-leave-active {
+  transition: all 0.3s;
 }
 </style>
